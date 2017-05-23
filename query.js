471410789b6d7b6ad8a2d46cf772df662c07aab2
Alysia Lynn <itsalysialynn-GitHub@gmail.com>
2017-05-23 20:46:50 +0000

    switched to knex

--- query.js
+++ query.js
@@ -1,33 +1,28 @@
-const pg = require("pg");
 const settings = require("./settings");
+require('dotenv').config();
+const client = require('knex')({
+  client: 'pg',
+  connection: process.env.PG_CONNECTION_STRING,
+  searchPath: 'knex,public'
+});
 
-const dbConfig = {
-  user: 'vagrant',
-  password: 'vagrant'
-};
-const client = new pg.Client( dbConfig );
-var args = process.argv.slice(2);
+const args = process.argv.slice(2);
 const input = process.argv[2];
 
 function padTwoDigitNumber(number) {
   return number < 10 ? '0' + number.toString() : number;
 }
 
-client.connect((err) => {
+client.select().from('famous_people').where('first_name', `${input}`).orWhere('last_name', `${input}`).asCallback(function (err, rows) {
   if (err) {
-    return console.error("Connection Error", err);
-  }
-  client.query("SELECT * FROM famous_people WHERE first_name = $1 OR last_name = $1", [input], (err, result) => {
-    if (err) {
-      return console.error("error running query", err);
-    }
-    console.log('Found', result.rows.length, "person(s) by the name '" + input + "':");
-    // console.log(result.rows[0]); //output: 1
-    let resultStrings = result.rows.map(function turnRowIntoString(row) {
+    console.error(err);
+  } else {
+    console.log('Found', rows.length, "person(s) by the name '" + input + "':");
+    let resultStrings = rows.map(function turnRowIntoString(row) {
       let birthdate = new Date(row.birthdate);
       return '- ' + row.id + ": " + row.first_name + ' ' + row.last_name + ", born '" + birthdate.getFullYear() + '-' + padTwoDigitNumber(birthdate.getMonth() + 1) + '-' + padTwoDigitNumber(birthdate.getDate()) + "'";
     });
     console.log(resultStrings.join('\n'));
-    client.end();
-  });
+  }
 });
+
